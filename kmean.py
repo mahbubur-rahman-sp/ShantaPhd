541b798230642102a5c431e9f0dee4e982e81339
SHAILA SHARMEEN <ssharmee@deakin.edu.au>
2018-11-22 22:51:32 +1100

    Kmean with random forest

--- kmean.py
+++ kmean.py
@@ -1,33 +1,62 @@
 import pandas as pd  
 import numpy as np  
-from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split 
+from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
+from sklearn.svm import SVC, LinearSVC  
 from sklearn.metrics import classification_report, confusion_matrix  
+from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import euclidean_distances
+from boruta import BorutaPy
+
 
 bankdata = pd.read_csv("perm_call_feature.csv")
 
 X = bankdata.drop('IS_MALWARE', axis=1)  
 y = bankdata['IS_MALWARE'] 
 
+clusters = 8
+
 model = KMeans(algorithm='auto', copy_x=True, init='k-means++', max_iter=600,
-    n_init=10, n_jobs=1, precompute_distances='auto',
+    n_init=10, n_jobs=1, precompute_distances='auto', n_clusters=clusters,
     random_state=None, tol=0.0001, verbose=0)
 model.fit(X)
+alldistances = model.fit_transform(X)
 
-k_means_labels = model.labels_
-k_means_cluster_centers = model.cluster_centers_
-dists = euclidean_distances(k_means_cluster_centers)
+for i in range(clusters):
+    X['d'+str(i)] = pd.Series(alldistances[:,i])
+   
 
-print(dists)
+forest = RandomForestClassifier(n_jobs=-1)
+#forest = ExtraTreesClassifier(n_estimators=50)
+
+feat_selector = BorutaPy(forest, n_estimators='auto', verbose=2, random_state=1)
+feat_selector.fit(X.as_matrix(), y.as_matrix())
+
+X_new = feat_selector.transform(X.as_matrix())
 
 
-labels = model.predict(X)
-for i in range(len(labels)):
-    labels[i] =  labels[i] 
+# rfe = RFE(estimator= svc, n_features_to_select= 50, step= 3).fit(X,y)
+# X_new = rfe.transform(X)
+#X_new = X
+
+print(X_new.shape)
+X_train, X_test, y_train, y_test = train_test_split(X_new, y, test_size = 0.20)  
 
 
-print(min(labels) )   
-print(max(labels) )   
-print(confusion_matrix(y,labels))
-print(classification_report(y,labels))  
+
+forest.fit(X_train, y_train)  
+
+y_pred = forest.predict(X_test)  
+print(confusion_matrix(y_test,y_pred))  
+print(classification_report(y_test,y_pred))
+
+# labels = model.predict(X)
+# for i in range(len(labels)):
+#     labels[i] =  labels[i] 
+
+
+# print(min(labels) )   
+# print(max(labels) )   
+# print(confusion_matrix(y,labels))
+# print(classification_report(y,labels))  
